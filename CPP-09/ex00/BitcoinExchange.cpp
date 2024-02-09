d3739fd3ce6e0915b027e3f6918e441ca3e46fef
m-bachar <m.aminebachar@gmail.com>
2024-02-09 02:35:18 +0100

    Parsing done, and other hilarious jokes i can tell myself :(

--- CPP-09/ex00/BitcoinExchange.cpp
+++ CPP-09/ex00/BitcoinExchange.cpp
@@ -20,16 +20,14 @@ int	BitcoinExchange::readFile(std::string filename)
 {
 	std::ifstream file(filename);
 	char	c;
-	if (!file.is_open())
-	{
+
+	if (!file.is_open()) {
 		std::cout << RED << " * Error : " << RESET << "unable to open or locate the file." << std::endl;
 		return 1;
 	}
-	while (file.get(c))
-	{
-		if (c == '\n')
-		{
-			std::cout << ORANGE << " * Line = " << RESET << line << std::endl;
+	while (file.get(c)) {
+		if (c == '\n') {
+			parseLine(line);
 			line.clear();
 		}
 		else
@@ -52,12 +50,38 @@ void	BitcoinExchange::parseLine(std::string line)
 
 void	BitcoinExchange::parseDateValue(std::string date, std::string value, int flag)
 {
-	if (flag == 0)
-	{
-		std::cout << ORANGE << " * Date = " << RESET << date << std::endl;
-		std::cout << ORANGE << " * Value = " << RESET << value << std::endl;
-		if (!std::strcmp(date.c_str(), "date") || !std::strcmp(value.c_str(), " | value"))
+	bool	errors_flag = false;
+	if (flag == 0) {
+		if (date != "date " || value != "| value") {
 			std::cout << RED << " * Error : " << RESET << "date and value must be as follow (date | value)" << std::endl;
+			errors_flag = true;
+		}
+	}
+	else {
+		if (date < "2009-01-02" || date > "2022-03-29") {
+			std::cout << RED << " * Error : " << RESET << "date " << date << "is out of range" << std::endl;
+			errors_flag = true;
+		}
+		std::string	value_sub = value.substr(value.find_first_of(' ') + 1, value.find_last_of('\n'));
+		int	dot_counter = 0;
+		for (size_t i = 0; i < value_sub.length(); i++) {
+			if (value_sub[0] == '-' || value_sub[0] == '+')
+				i++;
+			if (value_sub[i] == '.')
+				dot_counter++;
+			if ((!std::isdigit(value_sub[i]) && value_sub[i] != '.') || dot_counter > 1) {
+				std::cout << RED << " * Error : " << RESET << value_sub << " not a digit" << std::endl;
+				errors_flag = true;
+				break;
+			}
+		}
+		if (std::atof(value_sub.c_str()) < 0 || std::atof(value_sub.c_str()) > 1000) {
+			std::cout << RED << " * Error : " << RESET << value_sub << " must be between (0 - 1000)" << std::endl;
+			errors_flag = true;
+		}
+		// if (errors_flag) { // Insert db and input in map and dreb chi f chi htel eand molchi :D
+
+		// }
 	}
 }
 
